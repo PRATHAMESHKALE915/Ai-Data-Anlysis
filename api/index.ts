06733d1530ae543f72e1b0a1e0e5a81ed11959dd
PRATHAMESHKALE915 <prathameshkale915@gmail.com>
2026-08-08 23:38:31 +0530

    Add diagnostic error reporter to api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,13 @@
 import handler from "../server";
 
-export default handler;
+export default async function (req: any, res: any) {
+  try {
+    return await handler(req, res);
+  } catch (err: any) {
+    return res.status(500).json({
+      error: "API Handler Exception",
+      message: err?.message || String(err),
+      stack: err?.stack || null
+    });
+  }
+}
